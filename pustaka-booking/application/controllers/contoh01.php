09b2dfde55d4f7e24821ee5525526cabdb3b35f1
armasprt <armadika.design@gmail.com>
2022-09-19 16:18:33 +0700

    new file and coba coba

--- pustaka-booking/application/controllers/contoh01.php
+++ pustaka-booking/application/controllers/contoh01.php
@@ -1,10 +1,11 @@
 <?php
-class Contoh01 extends CI_Controller
-{
-    public function index()
-    {
+class Contoh01 extends CI_Controller {
+    public function index(){
+        echo "<div align='center'>";
         echo "<h1>Perkenalkan</h1>";
-        echo "Nama saya Armadika Supriatna Saya tinggal di cikarang selatan saya menyukai bola voli";
+        echo "<p>Nama saya adalah Bjorka, saya sudah meretas data keamanan negara</p>";
+        echo "<h2 style='color:red;'><marquee><strong>!!Jaga Data Pribadi Anda!!</strong><marquee></h2>";
+        echo "</div>";
     }
 }
 ?>
---
+++ pustaka-booking/application/views/view_coba.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome anjay</title>
+</head>
+<body>
+    <H1><marquee behavior="" direction="left">Welcome to Code Igniter</marquee></H1>
+    <h1>Perkenalkan</h1>
+    <p>Saya adalah coki the satan</p>
+</body>
+</html>
