883e897a9f9e5c3120721feb51010e2d31a92144
armasprt <armadika.design@gmail.com>
2022-09-24 10:55:40 +0700

    hapus index

--- pustaka-booking/application/views/view_latihan1.php
+++ pustaka-booking/application/views/view_latihan1.php
@@ -5,11 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan 1</title>
+    <style>
+        body {
+            color: #090000;
+            font-family: 'Poppins';
+        }
+    </style>
 </head>
 <body>
-    Halo Kawan... yuk kita belajar web programming...!!<br>
-    Nilai 1 = <?= $nilai1; ?>
-    Nilai 2 = <?= $nilai2; ?>
-    Ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1. " + ". $nilai2. " = ". $hasil; ?>
+    <h1>Halo Kawan... yuk kita belajar web programming...!!</h1>
+    Nilai 1 = <?= $nilai1; ?></br>
+    Nilai 2 = <?= $nilai2; ?></br>
+    Hasil dari pemodelan dengan method penjumlahan yaitu <?= $nilai1. " + ". $nilai2. " = ". $hasil; ?>
 </body>
 </html>
