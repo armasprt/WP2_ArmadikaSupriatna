cda3bf3e72c2281e693bd150ecefecdc158f7ae7
armasprt <armadika.design@gmail.com>
2022-10-03 16:15:31 +0700

    pembaruan css

--- pustaka-booking/application/views/view_header.php
+++ pustaka-booking/application/views/view_header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web Prog II | Merancang Template Sederhana Dengan Codeigniter</title>
-    <link rel="stylesheet" type="text/css" href="http://localhost/WP2_ArmadikaSupriatna/pustaka-booking/assets/css/stylebuku.css">
+    <link rel="stylesheet" type="text/css" href="http://localhost/WP2_ArmadikaSupriatna/pustaka-booking/assets/css/style.css">
 </head>
 <body>
     <div id="wrapper">
